d51173aad1b05a9a1f3174d7dc74da0b5f346a6c
Muskan Tripathi <62746972+MuskanTripathi@users.noreply.github.com>
2021-10-02 19:52:39 +0530

    Create hello_world_MuskanTripathi.cpp
    Hello world in 'cpp'

---
+++ scripts/hello_world_MuskanTripathi.cpp
@@ -0,0 +1,9 @@
+// LANGUAGE: Cpp
+// AUTHOR: Muskan Tripathi
+// GITHUB: https://github.com/MuskanTripathi
+#include<iostream.h>
+using namespace std;
+int main(){
+  cout<<"Hello World!";
+  return 0;
+}
